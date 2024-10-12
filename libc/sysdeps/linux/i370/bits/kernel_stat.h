518d54f372c70b4309c97aca1898814b994c6b44
Linas Vepštas <linasvepstas@gmail.com>
2024-10-12 13:52:44 -0500

    We need struct stat too

---
+++ libc/sysdeps/linux/i370/bits/kernel_stat.h
@@ -0,0 +1,26 @@
+#ifndef _BITS_STAT_STRUCT_H
+#define _BITS_STAT_STRUCT_H
+
+/* This file provides whatever this particular arch's kernel thinks
+ * struct stat should look like...  It turns out each arch has a
+ * different opinion on the subject... */
+
+struct kernel_stat {
+	unsigned long st_dev;
+	unsigned long st_ino;
+	unsigned short st_mode;
+	unsigned short st_nlink;
+	unsigned short st_uid;
+	unsigned short st_gid;
+	unsigned short st_rdev;
+	unsigned long  st_size;
+	unsigned long  st_blksize;
+	unsigned long  st_blocks;
+	struct timespec st_atim;
+	struct timespec st_mtim;
+	struct timespec st_ctim;
+	unsigned long     __unused4;
+	unsigned long     __unused5;
+};
+
+#endif	/*  _BITS_STAT_STRUCT_H */

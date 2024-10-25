d44421807e8dcaf1b88684e130c6c49ffd34952e
Linas Vepštas <linasvepstas@gmail.com>
2024-10-25 12:10:09 -0500

    Seems dirent64 is used uncondtionally.

--- libc/sysdeps/linux/i370/bits/dirent.h
+++ libc/sysdeps/linux/i370/bits/dirent.h
@@ -1,4 +1,4 @@
-/* The i370 uses teh older linux kernel 2.2.1 version */
+/* The i370 uses the older linux kernel 2.2.1 version */
 
 #ifndef _BITS_DIRENT_H
 #define _BITS_DIRENT_H	1
@@ -17,13 +17,20 @@ struct dirent
    __ILP32_OFFBIG_CFLAGS to -D_FILE_OFFSET_BITS=64 which is
    semi-correct cause we have 64-bit offsets in the kernel,
    but just not the general dirent. Anyway, __USE_LARGEFILE64
-   gets set as a result and I can't turn it off. */
+   gets set as a result and I can't turn it off.
+
+   Here's the next gotcha: The libc/misc/dirent/Makefile.in builds
+   libc/misc/dirent/readdir64.c unconditionally. And that file
+   says #if __WORDSIZE != 64 # define __DIRENT_TYPE struct dirent64
+   which seems wtf to me, but whatever, I will not argue with the
+   maintainers. Thus, this dirent must be same as above, for things
+   to actually work.
+ */
 struct dirent64
   {
-    __ino64_t d_ino;
-    __off64_t d_off;
+    long d_ino;
+    __off_t d_off;
     unsigned short int d_reclen;
-    unsigned char d_type;
     char d_name[256];
   };
 #endif

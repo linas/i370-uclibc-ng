410c65e0d121b74d0099769092bab83d141032ec
Linas Vepštas <linasvepstas@gmail.com>
2024-10-25 12:47:17 -0500

    The i370 uses an old compiler. Doesn't have new stuff.

--- libc/sysdeps/linux/i370/bits/wordsize.h
+++ libc/sysdeps/linux/i370/bits/wordsize.h
@@ -1,1 +1,10 @@
+/* The i370 arch is a 32-bit arch. */
 #define __WORDSIZE 32
+
+/* The currently available compilers for i370 are gcc-3.2.3 and
+ * gcc-3.4.6 and the open watcom compiler. The gcc's, at least,
+ * do not have a built-in `true` keyword. It's not in C90, at least.
+ */
+#if (defined __GNUC__  && __GNUC__ < 4)
+#define true 1
+#endif

4d911e1be714deb166e836ad366c60ad0c5a5631
Linas Vepštas <linasvepstas@gmail.com>
2024-10-25 23:31:27 -0500

    Add i370/sys/user.h

---
+++ libc/sysdeps/linux/i370/sys/user.h
@@ -0,0 +1,13 @@
+
+#ifndef _SYS_USER_H
+#define _SYS_USER_H	1
+
+/* This defines the structure of a core file for i370. Used by
+   binutils bfd, GDB, strace and maybe some rando core-dump tools.
+   Since the i370 kernel provides is, we'll just use that and cross
+   our fingers.
+ */
+
+#include <asm/user.h>
+
+#endif  /* _SYS_USER_H */

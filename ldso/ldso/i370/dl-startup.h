1dadbe5373cb7b8f53737a1a9fb2974598b285d2
Linas Vepštas <linasvepstas@gmail.com>
2024-11-05 13:58:39 -0600

    Stub in basic _start glue.

--- ldso/ldso/i370/dl-startup.h
+++ ldso/ldso/i370/dl-startup.h
@@ -2,6 +2,35 @@
  * Architecture specific code used by dl-startup.c
  */
 
+/* Why is this here, instead of some *.S file ? */
+__asm__(
+    " .section .text\n"
+    ".globl   _start\n"
+    " .type _start,@function\n"
+    " .balign 4\n"
+    "_start:\n"
+    " BASR    r6,0\n"              /* Figure out where we are. */
+    " .using .,r6\n"               /* Establish addressing. */
+    " ST      r1,0(r11)\n"         /* r1 holds SP, r11 holds FP. */
+    " L       r15,=V(_dl_start)\n"
+    " BASR    r14,r15\n"           /* Perform relocation. */
+
+/* TODO: I guess 0(r11) should be _GLOBAL_OFFSET_TABLE_ ?? */
+    " L       r2,0(r2)\n"          /* Get value of argc. */
+    " ST      r2,92(,r11)\n"       /* Pass argc on stack. */
+    " ST      r3,96(,r11)\n"       /* Pass argv on stack. */
+    " ST      r4,100(,r11)\n"      /* Pass envp on stack. */
+    " ST      r5,104(,r11)\n"      /* Something something aux vector. */
+    " BASR    r14,r15\n"           /* r15 now holds relocated main()\n. */
+
+/* TODO: call _dl_fini */
+
+    " .ltorg\n"
+    " .size _start,.-_start\n"
+    " .previous\n"
+);
+
+
 /* Get a pointer to the argv array.  On many platforms this can be just
  * the address of the first argument, on other platforms we need to
  * do something a little more subtle here.  */

a691023869f624d9dd71d22746ecd37a05125422
Linas Vepštas <linasvepstas@gmail.com>
2024-11-05 14:54:01 -0600

    Fake faccessat() for i370

--- libc/sysdeps/linux/common/faccessat.c
+++ libc/sysdeps/linux/common/faccessat.c
@@ -14,4 +14,16 @@ _syscall4(int, faccessat, int, fd, const char *, file, int, type, int, flag)
 libc_hidden_def(faccessat)
 #else
 /* should add emulation with faccess() and /proc/self/fd/ ... */
+
+/* For i370, there's no __NR_faccessat so I'm going to fake it.
+   At this time, the only caller is euidaccess() used by the dynamic
+   loader (to see if the app can be run) Its called with AT_FDCWD.
+   I'm gonna ignore the issues here.
+ */
+int faccessat(int fd, const char * file, int type, int flag)
+{
+  return access(file, type);
+}
+libc_hidden_def(faccessat)
+
 #endif

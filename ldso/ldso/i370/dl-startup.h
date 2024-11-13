f3a478ce4e244774869001ea9c56897e86031e1c
Linas Vepštas <linasvepstas@gmail.com>
2024-11-13 01:03:48 -0600

    Add design notes

--- ldso/ldso/i370/dl-startup.h
+++ ldso/ldso/i370/dl-startup.h
@@ -5,10 +5,55 @@
 /* Why is this here, instead of some *.S file ? */
 
 /* Below is an attempt, but it doesn't make it out the gates
-   because the current linker doesn't relocate the .ltorg.
-   Not sure why; other stand-alones don't have this problem.
-   At any rate, cannot even call into C code, yet, not until
-   the linker is fixed.
+   because ... I'm lazy.
+
+   The core issue is that PIC code with the current i370 ABI uses
+   not one, but two base registers: one for the text segment, so that
+   branch targets can be computed as offsets, and another, for the
+   data segment, where the literal pool and the page table reside.
+   This makes the text segment position independent, while all the
+   relocation targets reside in the pool (thus making the text segment
+   shareable, whereas each instance gets its own copy of the pool.)
+
+   Its easy to find out where one is loaded: just have _start issue
+   BASR rN,0 and rN now holds the current address. If _start is in the
+   .text segment, then we know where everything else is in the text
+   segment, e.g. by computing the relative address _dlstart - . and
+   adding it to the BASR result. We can jump to it. Great!  But then,
+   where is the pool?
+
+   Alternately, put _start in the .data segment, issue BASR and now
+   we know where the .data segment got loaded (and so we know where
+   the pools are, e.g. by computing the relative address
+   _dl_start$pool - . Great! But where's the text sgement?
+
+   There's two ways to solve this. One is to walk the .dynsym section
+   in assembly code, right here, and search for _dl_start$pool. Yuck.
+   (At least one of the other arches does this.) The search cannot be
+   done in C code because the C code will inevitably use the pool,
+   whose address we don't know. Not much is needed: only the addr of
+   _dl_start$pool as a minimum, and may as well get _dl_start$pgt
+   and _dl_start$text since we're at it. Plug em into the plt stub
+   and we're good to go.
+
+   The other way to solve this requires a weird linker trick. This
+   would place a copy of the pool in the text segment as well as in
+   .data.pool. This will make all literals in the pool accessible to
+   the C code; function addresses in the pool will be garbage, so
+   function calls through it are not allowed. Then, hunt down the
+   .data.pool section in C code, relocate it correctly, and then
+   switch to using that, instead of the temporary .text pool. Maybe
+   the -Bsymbolic flag in the linker could be used to set this up.
+
+   The third solution is to revise the assembler to keep two literal
+   pools: one for constants, since these never need relocation, and
+   can safely live in the text sgement. The other pool would hold
+   anything relocatable. This is perhaps the cleanest solution. Of
+   course, on entry, the .daata.pool still needs to be relocated,
+   just as it needs to be done in all three scenarios. But at least
+   this avoids having to walk .dlsym in assembly.
+
+   I didn't expect the Spanish Inquisition.
  */
 __asm__(
     " .section .text\n"
